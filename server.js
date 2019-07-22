d64fe9aa9269f5defc106ad2f34ab3dd7f85eea0
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-07-22 16:21:39 -0500

    added showindex page for store

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const storeController = require('./controllers/storeController')
 
 const app = express();
+
+
+//HOME PAGE
+app.get('/', (req,res) => {
+	res.render('index.ejs')
+})
+
+app.use("/stores", storeController)
+
 
 app.listen(3000, () => {
   console.log('my server is listening for client requests on port 3000')
